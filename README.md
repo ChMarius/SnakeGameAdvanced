8d4b950bb56360f181f976b545bb6201981507a2
Chirita Marius <chiritamarius818@gmail.com>
2024-08-19 15:52:42 +0200

    Score resseting issue fixed

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Snake-Game
+
+The project started from a youtube tutorial, when I started learning C++, but over time I added some features like levels, map expansion, etc.
+
+Things to fix: 
+1. Pace variable for every level increase
+2. The score reset after every level increase (DONE)
+3. Map increasing needs restructuring
--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ int x,y,fruitX,fruitY,Score;
 int tailX[100], tailY[100];
 int bombX,bombY;
 int nTail;
-int pace=60;
+int pace=150;
 int nrBombs=2;
 enum eDirection {Stop=0,Left,Right,Up,Down};
 eDirection dir;
@@ -23,14 +23,16 @@ bool introToGame()
 {
     char choice;
     // Introduces the player to the game
-    if(!gameOver){
-    std::cout<<"Welcome to the Snake Game!"<<std::endl;
-    std::cout<<std::endl;
-    std::cout<<"Would you like to play to play the game? Type Y for yes and N for no "<<std::endl;
+    if(!gameOver)
+    {
+        std::cout<<"Welcome to the Snake Game!"<<std::endl;
+        std::cout<<std::endl;
+        std::cout<<"Would you like to play to play the game? Type Y for yes and N for no "<<std::endl;
     }
     else if (exitByChoice) {std::cout<<"You exited the game! Would you like to play again? Type Y for yes and N for no"<<std::endl;}
     else {std::cout<<"Oops, you lost. Would you like to play again? Type Y for yes and N for no"<<std::endl;}
-    while(true){
+    while(true)
+    {
         std::cout<<">"; std::cin>>choice;
         if(toupper(choice)=='Y') {return true;}
         if(toupper(choice)=='N'){ 
@@ -38,7 +40,7 @@ bool introToGame()
             return false;}
         // If input is valid, it returns the player to the first line of the while loop to place an input again
         else {std::cout<<"Invalid Input. Please choose Y or N"<<std::endl; continue; }
-        }
+    }
 }
 
 
@@ -48,8 +50,8 @@ void Setup()
 {
     // Everytime we press to start the game again after losing, we reset the tail
     nTail=0;
-    tailX[0]=0;tailY[0]=0;
-    // Placement of the bomb is done randomly
+    tailX[0]=0;
+    tailY[0]=0;
     width=10; 
     height=10;
     // Placement of the head is done in the center
@@ -62,21 +64,23 @@ void Setup()
     fruitY=rand()%height;
     Score=0;// Everytime the snake catches the a fruit, we increment the score with 1
     level=2;
+    // Placement of the bomb is done randomly
     bombX=rand()%width;
     bombY=rand()%height;
-    if(bombX==fruitX && bombY==fruitY && bombX==x && bombY==y){
-        fruitX=rand()%width;
-        fruitY=rand()%height;
+    if((bombX==fruitX && bombY==fruitY) || (bombX==x && bombY==y)) // If the bomb is in the same coordinates as the snake or the fruit, its coordinates are changed
+    {
+        bombX=rand()%width;
+        bombY=rand()%height;
     }
     exitByChoice=0;
 }
 
 void LevelCheck()
 {
-    if(level>2 && isChanged==0){
+    if(level>2 && isChanged==0)
+    {
         height+=3;             //*=level-1;
         width+=6;             //*=level-1;
-        Score=0;
         pace-=8;
         isChanged=1;
     }
@@ -89,8 +93,10 @@ void Draw()
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), {0,0});
     for(int j=0;j<=width+1;j++) {std::cout<<"#";}// The first # is 0 and the last # is 22, in order to have the width=20
     std::cout<<std::endl;
-    for (int i=0;i<=height;i++){
-        for(int j=0;j<=width;j++){ 
+    for (int i=0;i<=height;i++)
+    {
+        for(int j=0;j<=width;j++)
+        { 
             if(j==0) {std::cout<<"#";} // Prints the left wall
             if(i==y && j==x) {std::cout<<"O";} // Prints the head of the snake
             else if(i==fruitY && j==fruitX) {std::cout<<"F";}
@@ -98,10 +104,9 @@ void Draw()
             else 
             {
                 bool print=false;
-                for(int k=0;k<nTail;k++){
-                    
-                    if(tailX[k]==j && tailY[k]==i) {std::cout<<"o"; print=true;}// Prints the tail if coordinates of tailX and tailY (from logic function) are equal to i and j
-                    
+                for(int k=0;k<nTail;k++)
+                {
+                    if(tailX[k]==j && tailY[k]==i) {std::cout<<"o"; print=true;}// Prints the tail if coordinates of tailX and tailY (from logic function) are equal to i and j   
                 }
                 if(!print) {std::cout<<" ";}// Prints the whole 20x20 terrain
             } 
@@ -114,7 +119,6 @@ void Draw()
     std::cout<<"Score:"<<Score<<std::endl;
     std::cout<<"Level:"<<level-1<<std::endl;
 }
-
 
 // Assignemt of keys for their corresponding directions and the exit key
 void Input()
@@ -144,12 +148,15 @@ void Input()
      }
    }
 }
+
 void Logic()
 {
     int prevX=tailX[0]; int prevY=tailY[0]; 
     int prev2X, prev2Y;
-    tailX[0]=x; tailY[0]=y;
-    for(int i=1;i<nTail;i++){
+    tailX[0]=x; 
+    tailY[0]=y;
+    for(int i=1;i<nTail;i++)
+    {
         prev2X=tailX[i]; prev2Y=tailY[i];
         tailX[i]=prevX; tailY[i]=prevY;
         prevX=prev2X; prevY=prev2Y;
@@ -174,36 +181,45 @@ void Logic()
          dir=Stop;
          break;
     }
-    if(x>=width || x<0 || y>=height+1 || y<0 || bombX==x || bombY==y) {gameOver=true;} // If the snake crosses the border, the game stops
-    for(int i=0;i<nTail;++i){
-        if(tailX[i]==x && tailY[i]==y ) {gameOver=true;} //If the snake touches itself, the game stops
+    if(x>=width || x<0 || y>=height+1 || y<0 || (bombX==x && bombY==y)) {gameOver=true; std::cout<<"Stop because border cross or bomb touched";} // If the snake crosses the border, the game stops
+    for(int i=0;i<nTail;++i)
+    {
+        if(tailX[i]==x && tailY[i]==y ) {gameOver=true; std::cout<<"Stop because snake touched itself";} //If the snake touches itself, the game stops
     }
     
-    if(x==fruitX && y==fruitY ){ // Everytime the snake eats the fruit, the tail is incremented and the fruit is randomly placed
+    if(x==fruitX && y==fruitY )
+    // Everytime the snake eats the fruit, the tail is incremented and the fruit is randomly placed
+    {
         nTail++;
         Score++;
         fruitX=rand()%width;
         fruitY=rand()%height;
-        
-        if(Score%5==0) {level++; isChanged=0;}
-
+        if(Score%5==0) 
+        {
+            level++; 
+            isChanged=0;
+        }
     } 
-    
-
 }
+
 int main()
 {
-     //If "introToGame" returns true--> the game starts, if it returns false--> the terminal stops
-     while(introToGame()){
-      system("cls");
-      Setup();
-      while(!gameOver){
-        LevelCheck();
-        Draw();
-        Input();
-        Logic();
-        Sleep(pace); // Slows the pace of the snake
+    //If "introToGame" returns true --> the game starts, if it returns false --> the program stops
+    while(introToGame())
+    {
+        system("cls");
+        Setup();
+        while(!gameOver)
+        {
+            if(level<=5)
+            {
+              LevelCheck();
+            }
+            Draw();
+            Input();
+            Logic();
+            Sleep(pace); // Slows the pace of the snake
+        }   
+        //if(gameOver) {system("cls");}
     }
-    if(gameOver){system("cls");}
-  }
 }
